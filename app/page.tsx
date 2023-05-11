1bcfecd0ea4658bb82ff72cca89e8c573d9acfa4
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-11 11:15:59 -0400

    debug

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
-
+import Head from 'next/head';
 import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#0554af] to-[#6e93d9] p-10 flex flex-col justify-center items-center">
+            <Head>
+        <title>Weather AI</title>
+        <meta name="description" content="Get weather information powered by OpenAI" />
+      </Head>
       <Card className="max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">
